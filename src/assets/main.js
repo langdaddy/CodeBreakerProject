eb733fe76186a1affb243becb2abf17499a19808
Keith Lang <kllang@southernco.com>
2017-08-03 09:00:54 -0400

    Complete Class

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,10 +1,7 @@
 //let answer = document.getElementById('answer');
 //let attempt = document.getElementById('attempt');
 
-//function guess() {
-   // let input = document.getElementById('user-guess');
-    //add functionality to guess function here
-//}
+
 
 //implement new functions here
 
